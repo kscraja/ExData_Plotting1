16f272033e53ff409044a9a4fbc30d035fb4d7fe
Chandra Koduru <kscraja@gmail.com>
2014-06-22 09:46:44 +0530

    finished plot5

---
+++ project2/plot5.R
@@ -0,0 +1,32 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+NEI$fips <- as.factor(NEI$fips)
+NEI$SCC <- as.factor(NEI$SCC)
+NEI$Pollutant <- as.factor(NEI$Pollutant)
+NEI$type <- as.factor(NEI$type)
+
+# Across the United States, how have emissions 
+# from coal combustion-related sources changed from 1999–2008?
+
+
+# motor vehicles SCC Types
+ccSCC <- subset(SCC, SCC.Level.One == "Mobile Sources")
+
+# selecting only baltmore data
+# bmData <- subset(NEI, (fips == "24510"))
+# oldBMData <- bmData
+# selecting only baltmore data and coal combustion
+bmData <- subset(NEI, (fips == "24510" & SCC %in% ccSCC$SCC))
+
+splitTotalsPerYear <- split(bmData, list(bmData$year))
+
+yearlyEmissionTotals <- sapply(splitTotalsPerYear, function(d) {
+    sum(d$Emissions)
+})
+
+png(file="plot5.png")
+plot(names(yearlyEmissionTotals), yearlyEmissionTotals, pch=19, type="l",
+     xaxt="n", xlab="Year", ylab="Total Emission", main="Yearly Total Emissions - Baltimore - Motor vehicles")
+axis(1, at=names(yearlyEmissionTotals))
+dev.off()
